9137c309019068628ae56057c1195831c2af2d6d
manoj <manoj@alagesan.fr>
2021-04-12 10:00:45 +0200

    montecarlo création noeud et JeuAleatoireMT

--- src/tictactoecodingame/Noeud.java
+++ src/tictactoecodingame/Noeud.java
@@ -1,4 +1,13 @@
 package tictactoecodingame;
 
+import java.util.List;
+
 public class Noeud {
+    Noeud parent; //renvoie le noeud précédent
+    List<Noeud> noeudsenfant; // renvoie la liste des noeuds enfants
+    int nbpartiesouscenoeud; // nombre de partie lancer sous le noeud
+    int nbVictoirenoeud; // nombre de victoire sur le noeud
+
 }
+
+
