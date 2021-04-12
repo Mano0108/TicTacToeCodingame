99bd8261a0ee1e4a916a84ef60c596622ff09596
Ourithi <75417166+Ourithi@users.noreply.github.com>
2021-04-12 14:21:49 +0200

    Update JeuAleatoireMC.java

--- src/tictactoecodingame/JeuAleatoireMC.java
+++ src/tictactoecodingame/JeuAleatoireMC.java
@@ -1,4 +1,16 @@
 package tictactoecodingame;
+import java.util.Random;
 
 public class JeuAleatoireMC {
+  public int partie_rd(Plateau plateau, Joueur joueur){
+        while (!plateau.partieTerminee()){
+            ArrayList<Coup> coups = plateau.getListeCoups(joueur);
+            Random generator = new Random();
+            int randomIndex = generator.nextInt(coups.length);
+            Coup coup = coups.get(randomIndex);
+            plateau.joueCoup(coup);
+        }
+        return plateau.vainqueur();
+
+    }
 }
