91c1e5c044cfe80539b9f32be2de74766cf56993
Ourithi <75417166+Ourithi@users.noreply.github.com>
2021-04-13 02:25:05 +0200

    Update JeuAleatoireMC.java

--- src/tictactoecodingame/JeuAleatoireMC.java
+++ src/tictactoecodingame/JeuAleatoireMC.java
@@ -1,25 +1,59 @@
 package tictactoecodingame;
+
+import java.util.List;
 import java.util.Random;
 import java.util.ArrayList;
 
+public class AlgoRechercheMC {
+    //une methode , avec un while (nombre d'iterations) pour lancer des simulations (4 etapes; selection, expansion, simulation, update) pour trouver le prochain Coup
+    //une méthode pour chaque étape
+    //
+    int nombreiteration;
+    int nbSimulations;
+    ArbreMC arbre = new ArbreMC();
+    NoeudMC noeudRacine = ArbreMC.getRacine();
 
-public class JeuAleatoireMC {
-  public int partie_rd(Plateau plateau, Joueur currentJoueur, Joueur joueur1, Joueur joueur2){
-        while (!plateau.partieTerminee()){
-            ArrayList<Coup> coups = plateau.getListeCoups(currentJoueur);
+    public Coup meilleur_coup(Plateau plateau, Joueur currentJoueur, Joueur joueur1, Joueur joueur2){
+        
+
+    }
+    public static NoeudMC selection(NoeudMC noeud){
+        ArrayList<NoeudMC> noeuds = NoeudMC.getNoeudsEnfants();
+        if (noeuds.size() == 0){
+            return noeud;
+        }
+        else{
             Random generator = new Random();
-            int randomIndex = generator.nextInt(coups.size());
-            Coup coup = coups.get(randomIndex);
-            plateau.joueCoup(coup);
+            int randomIndex = generator.nextInt(noeuds.size());
+            NoeudMC nv_noeud = noeuds.get(randomIndex);
+            return selection(nv_noeud);
         }
-        if (plateau.vainqueur()==joueur1){
-            return 1;
-        }
-        else if (plateau.vainqueur()==joueur2){
-            return -1;
-        }
-        else {
-            return 0;
+
+    }
+    public static void expansion(NoeudMC noeud_ori, Joueur currentJoueur){
+       ArrayList coups = noeud_ori.plateau.getListeCoups(currentJoueur);
+       Plateau plateau = noeud_ori.plateau;
+       for(int k=0;k<coups.size();k++){
+           NoeudMC nv_noeud = new NoeudMC();
+           plateau.joueCoup((Coup) coups.get(k));
+           nv_noeud.plateau = plateau;
+           plateau.annuleDernierCoup();
+           ArbreMC.ajouterEnfant(noeud_ori, nv_noeud);
+       }
+    }
+    public static int simulation(NoeudMC noeud, Plateau plateau, Joueur currentJoueur, Joueur joueur1, Joueur joueur2){
+        ArrayList<NoeudMC> noeuds_enfants = noeud.getNoeudsEnfants();
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(noeuds_enfants.size());
+        NoeudMC noeud_simu = noeuds_enfants.get(randomIndex);
+        int res = JeuAleatoireMC.partie_rd(noeud_simu.plateau, currentJoueur, joueur1, joueur2);
+        return res;
+    }
+    public static void backpro(NoeudMC noeud, int res, ArbreMC arbre){
+        while(noeud != arbre.getRacine()){
+            noeud.nbSimulations++;
+            noeud.nbVictoire+= res;
+            noeud=noeud.parent;
         }
     }
 }
