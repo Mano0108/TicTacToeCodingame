e72734266e8edcf8cdd20574b6fb7781dc696247
manoj <manoj@alagesan.fr>
2021-04-13 10:04:41 +0200

    montecarlo fin jeux aleatoires

--- src/tictactoecodingame/JeuxAleatoireMC.java
+++ src/tictactoecodingame/JeuxAleatoireMC.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 
 public class JeuxAleatoireMC {
 
-    public int partie_rd(Plateau plateau, Joueur currentJoueur, Joueur joueur1, Joueur joueur2){
-        while (!plateau.partieTerminee()){
+    public int partie_rd(Plateau plateau, Joueur currentJoueur, Joueur joueur1, Joueur joueur2) {
+        while (!plateau.partieTerminee()) {
             ArrayList<Coup> coups = plateau.getListeCoups(currentJoueur);
             Random generator = new Random();
             int randomIndex = generator.nextInt(coups.size());
             Coup coup = coups.get(randomIndex);
             plateau.joueCoup(coup);
-            if (plateau.vainqueur()==joueur1){
+            if (plateau.vainqueur() == joueur1) {
                 return 1;
-            }
-            else if (plateau.vainqueur()==joueur2){
+            } else if (plateau.vainqueur() == joueur2) {
                 return -1;
-            }
-            else {
+            } else {
                 return 0;
             }
 
 
         }
+        return 0;
     }
+}
